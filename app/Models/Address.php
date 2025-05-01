d655ab78ce965609cb26fc7f1be3c3b4c3fab1f3
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-01 16:33:08 -0300

    04 - feat: config Model & Migration Address Brand Category Order OrderItem Product

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Order;
 
 class Address extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'first_name',
+        'last_name',
+        'phone',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getFullNameAttrsibute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+    
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'grand_total',
+        'payment_method',
+        'payment_status',
+        'status',
+        'currency',
+        'shipping_amout',
+        'shipping_method',
+        'notes',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
 }
