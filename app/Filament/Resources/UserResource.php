efc14f7b895e521f231cb1735aa85e45413edba8
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-01 18:20:49 -0300

    08 - feat: adjusting form & table UserResource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,20 +2,36 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
-use App\Models\User;
+use DateTime;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+use App\Models\User;
 use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Pages\Page;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Illuminate\Support\Facades\Hash;
+use Filament\Pages\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Resources\Pages\CreateRecord;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use App\Filament\Resources\UserResource\Pages\ListUsers;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\RelationManagers;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
+    
+    protected static ?string $label = 'Usuário';
 
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
 
@@ -23,7 +39,26 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->label('Nome')
+                    ->required(),
+
+                TextInput::make('email')
+                    ->label('E-mail')
+                    ->email()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true)
+                    ->required(),
+
+                DateTimePicker::make('email_verified_at')
+                    ->label('E-mail verificado em')
+                    ->default(now()),
+
+                TextInput::make('password')
+                    ->label('Senha')
+                    ->password()                    
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (Page $livewire): bool => $livewire instanceof CreateRecord)
             ]);
     }
 
@@ -31,13 +66,34 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Nome')    
+                    ->sortable(),
+
+                TextColumn::make('email')
+                    ->label('E-mail')
+                    ->sortable(),
+
+                TextColumn::make('email_verified_at')
+                    ->label('E-mail verificado em')
+                    ->dateTime()
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->dateTime()
+                    ->sortable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),                    
+                ])
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
