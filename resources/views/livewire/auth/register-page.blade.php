d3c46b03bc8c4c6b3f406cc1530539d165d1b6d8
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-15 14:27:42 -0300

    30 - fix: adjusting links using wire:navigate

--- resources/views/livewire/auth/register-page.blade.php
+++ resources/views/livewire/auth/register-page.blade.php
@@ -7,7 +7,7 @@
                         <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign up</h1>
                         <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
                             Already have an account?
-                            <a class="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            <a wire:navigate class="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                                 href="/login">
                                 Sign in here
                             </a>
@@ -81,7 +81,6 @@
                                 </p>
                             </div>
                             <!-- End Form Group -->
-
 
                             <button type="submit"
                                 class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Sign
