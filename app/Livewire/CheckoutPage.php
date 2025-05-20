cad3580dc8578c349baa02608945e5284bcf791d
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-20 14:48:58 -0300

    39 - feat: display data on checkout-page and add func placeOrder

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,44 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout - Ecommerce')]
 class CheckoutPage extends Component
 {
+    public $firstName;
+    public $lastName;
+    public $phone;
+    public $streetAddress;
+    public $city;
+    public $state;
+    public $zipCode;
+    public $paymentMethod;
+
+    public function placeOrder()
+    {
+        $this->validate([
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'phone' => 'required',
+            'streetAddress' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zipCode' => 'required',
+            'paymentMethod' => 'required',
+        ]);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cartItems = CartManagement::getCartItemsFromCookie();
+        $grandTotal = CartManagement::calculateGrandTotal($cartItems);
+
+        return view('livewire.checkout-page', [
+            'cartItems' => $cartItems,
+            'grandTotal' => $grandTotal
+        ]);
     }
 }
