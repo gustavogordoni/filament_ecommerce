9b1c256d95fd795689679dfa32eae7a596741ebe
gustavogordoni <gustavogordoni1@gmail.com>
2025-05-20 13:00:31 -0300

    37 - feat: func Login Register

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Login - Ecommerce')]
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+
+    public function save()
+    {
+        $this->validate([
+            'email' => 'required|email|exists:users,email|max:255',
+            'password' => 'required|min:8|max:255',
+        ]);
+
+        if(!auth()->attempt(['email' => $this->email, 'password' => $this->password])){            
+            session()->flash('error', 'Inavalid credentias.');
+            return;
+        }
+
+        return redirect()->intended();
+    }
+
     public function render()
     {
         return view('livewire.auth.login-page');
--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,38 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
+#[Title('Register - Ecommerce')]
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:8|max:255',
+        ]);
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->intended();
+    }
+
     public function render()
     {
         return view('livewire.auth.register-page');
